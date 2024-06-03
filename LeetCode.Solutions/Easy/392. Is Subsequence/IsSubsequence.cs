35352f62db0a631e70f981d0ba45b8676f0c7e36
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-06-03 12:42:24 +0300

    392. Is Subsequence

---
+++ LeetCode.Solutions/Easy/392. Is Subsequence/IsSubsequence.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Easy._392._Is_Subsequence
+{
+    public class IsSubsequence
+    {
+        public bool Solve(string s, string t)
+        {
+            if (s == string.Empty)
+                return true;
+
+            int sIndex = 0;
+            int tIndex = 0;
+
+            while (tIndex < t.Length)
+            {
+                if (t[tIndex] == s[sIndex])
+                {
+                    if (++sIndex == s.Length)
+                        return true;
+                }
+                tIndex++;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCode.Tests/Easy/392. Is Subsequence/IsSubsequenceTest.cs
@@ -0,0 +1,45 @@
+﻿using LeetCode.Solutions.Easy._392._Is_Subsequence;
+using LeetCode.Solutions.Medium._2486._Append_Characters_to_String_to_Make_Subsequence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Easy._392._Is_Subsequence
+{
+    public class IsSubsequenceTest
+    {
+        [Fact]
+        public void Solve_ReturnsCorrectIndices_Case1()
+        {
+            var isSubsequence = new IsSubsequence();
+            var result = isSubsequence.Solve("abc", "ahbgdc");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Solve_ReturnsCorrectIndices_Case2()
+        {
+            var isSubsequence = new IsSubsequence();
+            var result = isSubsequence.Solve("axc", "ahbgdc");
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Solve_ReturnsCorrectIndices_Case3()
+        {
+            var isSubsequence = new IsSubsequence();
+            var result = isSubsequence.Solve("", "ahbgdc");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Solve_ReturnsCorrectIndices_Case4()
+        {
+            var isSubsequence = new IsSubsequence();
+            var result = isSubsequence.Solve("acb", "ahbgdc");
+            Assert.False(result);
+        }
+    }
+}
