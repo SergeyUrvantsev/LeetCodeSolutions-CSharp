e23cb7247654bf36e0f06f0b05ac3fa1cd718cb5
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-06-03 16:06:30 +0300

    26. Remove Duplicates from Sorted Array

---
+++ LeetCode.Solutions/Easy/26. Remove Duplicates from Sorted Array/RemoveDuplicates.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Easy._26._Remove_Duplicates_from_Sorted_Array
+{
+    public class RemoveDuplicates
+    {
+        //Complexity
+        //Time: O(n), где n - длина массива nums.
+        //Space: O(1), в алгоритме используется постоянное количество дополнительной памяти,
+        //  независимо от размера входного массива. Bзменения выполняются непосредственно в массиве nums,
+        //  а не создаются дополнительные структуры данных или массивы.
+
+        public int Solve(int[] nums)
+        {
+            var uniqueElementsCount = 1;
+            var currentElement = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if(currentElement != nums[i])
+                {
+                    currentElement = nums[i];
+
+                    if(uniqueElementsCount <= i)
+                    {
+                        nums[uniqueElementsCount] = currentElement;
+                        uniqueElementsCount++;
+                    }
+                }
+            }
+
+            return uniqueElementsCount;
+        }
+    }
+}
---
+++ LeetCode.Tests/Easy/26. Remove Duplicates from Sorted Array/RemoveDuplicatesTest.cs
@@ -0,0 +1,52 @@
+﻿using LeetCode.Solutions.Easy;
+using LeetCode.Solutions.Easy._20._Valid_Parentheses;
+using LeetCode.Solutions.Easy._26._Remove_Duplicates_from_Sorted_Array;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Easy._26._Remove_Duplicates_from_Sorted_Array
+{
+    public class RemoveDuplicatesTest
+    {
+        [Fact]
+        public void Solve_ReturnsCorrectIndices_Case1()
+        {
+            // Arrange
+            RemoveDuplicates solution = new RemoveDuplicates();
+            var nums = new int[] { 1, 1, 2 };
+
+            // Act
+            var result = solution.Solve(nums);
+
+            // Assert
+            Assert.Equal(2, result);
+            AssertArrayContainsElements(nums, new int[] { 1, 2 });
+        }
+
+        [Fact]
+        public void Solve_ReturnsCorrectIndices_Case2()
+        {
+            // Arrange
+            RemoveDuplicates solution = new RemoveDuplicates();
+            var nums = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+
+            // Act
+            var result = solution.Solve(nums);
+
+            // Assert
+            Assert.Equal(5, result);
+            AssertArrayContainsElements(nums, new int[] { 0, 1, 2, 3, 4 });
+        }
+
+        private void AssertArrayContainsElements(int[] actual, int[] expected)
+        {
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.Equal(expected[i], actual[i]);
+            }
+        }
+    }
+}
