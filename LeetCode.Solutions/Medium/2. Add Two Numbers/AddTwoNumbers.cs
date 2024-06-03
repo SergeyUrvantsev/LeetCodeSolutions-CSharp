2395bff5515161d021c3b13b430ae2f07bb3190c
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-06-03 14:17:55 +0300

    2. Add Two Numbers

---
+++ LeetCode.Solutions/Medium/2. Add Two Numbers/AddTwoNumbers.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode.Solutions.Medium._2._Add_Two_Numbers
+{
+    public class AddTwoNumbers
+    {
+        public ListNode Solve(ListNode l1, ListNode l2)
+        {
+            ListNode dummyHead = new ListNode(0);
+            ListNode curr = dummyHead;
+            int carry = 0;
+
+            while (l1 != null || l2 != null || carry != 0)
+            {
+                int sum = carry;
+                if (l1 != null)
+                {
+                    sum += l1.val;
+                    l1 = l1.next;
+                }
+                if (l2 != null)
+                {
+                    sum += l2.val;
+                    l2 = l2.next;
+                }
+
+                curr.next = new ListNode(sum % 10);
+                carry = sum / 10;
+                curr = curr.next;
+            }
+
+            return dummyHead.next;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ LeetCode.Tests/Medium/2. Add Two Numbers/AddTwoNumbersTest.cs
@@ -0,0 +1,67 @@
+﻿using LeetCode.Solutions.Medium._2._Add_Two_Numbers;
+using LeetCode.Solutions.Medium._6._Zigzag_Conversion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Medium._2._Add_Two_Numbers
+{
+    public class AddTwoNumbersTest
+    {
+        [Fact]
+        public void Solve_ReturnsCorrectIndices_Case1()
+        {
+            // Arrange
+            ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+            ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+            var addTwoNumbers = new AddTwoNumbers();
+
+            // Act
+            var result = addTwoNumbers.Solve(l1, l2);
+
+            // Assert
+            Assert.Equal(7, result.val);
+            Assert.Equal(0, result.next.val);
+            Assert.Equal(8, result.next.next.val);
+        }
+
+        [Fact]
+        public void Solve_ReturnsCorrectIndices_Case2()
+        {
+            // Arrange
+            ListNode l1 = new ListNode(0);
+            ListNode l2 = new ListNode(0);
+            var addTwoNumbers = new AddTwoNumbers();
+
+            // Act
+            var result = addTwoNumbers.Solve(l1, l2);
+
+            // Assert
+            Assert.Equal(0, result.val);
+        }
+
+        [Fact]
+        public void Solve_ReturnsCorrectIndices_Case3()
+        {
+            // Arrange
+            ListNode l1 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))));
+            ListNode l2 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))));
+            var addTwoNumbers = new AddTwoNumbers();
+
+            // Act
+            var result = addTwoNumbers.Solve(l1, l2);
+
+            // Assert
+            Assert.Equal(8, result.val);
+            Assert.Equal(9, result.next.val);
+            Assert.Equal(9, result.next.next.val);
+            Assert.Equal(9, result.next.next.next.val);
+            Assert.Equal(0, result.next.next.next.next.val);
+            Assert.Equal(0, result.next.next.next.next.next.val);
+            Assert.Equal(0, result.next.next.next.next.next.next.val);
+            Assert.Equal(1, result.next.next.next.next.next.next.next.val);
+        }
+    }
+}
