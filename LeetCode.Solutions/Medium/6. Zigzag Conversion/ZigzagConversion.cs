289d3c57c6ea66bf7e09bce56c7dc476209bac8b
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-06-03 14:09:11 +0300

    6. Zigzag Conversion

---
+++ LeetCode.Solutions/Medium/6. Zigzag Conversion/ZigzagConversion.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Medium._6._Zigzag_Conversion
+{
+    public class ZigzagConversion
+    {
+        //Complexity:
+        //Time complexity: O(n), где n - длина строки. Каждый символ обрабатывается один раз.
+        //Space complexity: O(n), для хранения символов в строках.
+
+        public string Solve(string s, int numRows)
+        {
+            if (numRows == 1 || s.Length <= numRows)
+            {
+                return s;
+            }
+
+            StringBuilder[] rows = new StringBuilder[numRows];
+            for (int i = 0; i < numRows; i++)
+            {
+                rows[i] = new StringBuilder();
+            }
+
+            int curRow = 0;
+            bool goingDown = false;
+
+            foreach (char c in s)
+            {
+                rows[curRow].Append(c);
+                if (curRow == 0 || curRow == numRows - 1)
+                {
+                    goingDown = !goingDown;
+                }
+                curRow += goingDown ? 1 : -1;
+            }
+
+            StringBuilder result = new StringBuilder();
+            foreach (var row in rows)
+            {
+                result.Append(row);
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ LeetCode.Tests/Medium/6. Zigzag Conversion/ZigzagConversionTest.cs
@@ -0,0 +1,37 @@
+﻿using LeetCode.Solutions.Medium._2486._Append_Characters_to_String_to_Make_Subsequence;
+using LeetCode.Solutions.Medium._6._Zigzag_Conversion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Medium._6._Zigzag_Conversion
+{
+    public class ZigzagConversionTest
+    {
+        [Fact]
+        public void Solve_ReturnsCorrectIndices_Case1()
+        {
+            var zigzagConversion = new ZigzagConversion();
+            var result = zigzagConversion.Solve("PAYPALISHIRING", 3);
+            Assert.Equal("PAHNAPLSIIGYIR", result);
+        }
+
+        [Fact]
+        public void Solve_ReturnsCorrectIndices_Case2()
+        {
+            var zigzagConversion = new ZigzagConversion();
+            var result = zigzagConversion.Solve("PAYPALISHIRING", 4);
+            Assert.Equal("PINALSIGYAHRPI", result);
+        }
+
+        [Fact]
+        public void Solve_ReturnsCorrectIndices_Case3()
+        {
+            var zigzagConversion = new ZigzagConversion();
+            var result = zigzagConversion.Solve("A", 1);
+            Assert.Equal("A", result);
+        }
+    }
+}
