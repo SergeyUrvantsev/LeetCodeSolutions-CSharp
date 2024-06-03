20b6a94132b283d411f9245481b627da0b91d46c
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-06-03 15:09:27 +0300

    9. Palindrome Number

---
+++ LeetCode.Solutions/Easy/9. Palindrome Number/IsPalindrome.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Easy._9._Palindrome_Number
+{
+    public class IsPalindrome
+    {
+        //Complexity
+        //Time: O(log(n)), где n - количество цифр в числе x.
+        //  Это потому, что мы делим число x на 10 в каждой итерации цикла while,
+        //  что приводит к уменьшению числа цифр в x вдвое на каждой итерации.
+        //Space: O(n), так как мы используем дополнительную память для хранения обратной строки числа.
+
+        public bool Solve_v2(int x)
+        {
+            if (x < 0 || (x % 10 == 0 && x != 0))
+                return false;
+
+            int reversed = 0;
+            while (x > reversed)
+            {
+                reversed = reversed * 10 + x % 10;
+                x /= 10;
+            }
+
+            return x == reversed || x == reversed / 10;
+        }
+
+        //Complexity
+        //Time: O(n), где n - количество цифр в числе x.
+        //Space: O(n), так как мы используем дополнительную память для хранения обратной строки числа.
+
+        public bool Solve_v1(int x) =>
+            x.ToString() == new string(x.ToString().Reverse().ToArray());
+    }
+}
---
+++ LeetCode.Tests/Easy/9. Palindrome Number/IsPalindromeTest.cs
@@ -0,0 +1,48 @@
+﻿using LeetCode.Solutions.Easy._9._Palindrome_Number;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Easy._9._Palindrome_Number
+{
+    public class IsPalindromeTest
+    {
+        [Theory]
+        [InlineData(121, true)]
+        [InlineData(-121, false)]
+        [InlineData(10, false)]
+        [InlineData(12321, true)]
+        [InlineData(123456789, false)]
+        public void TestIsPalindrome_v1(int x, bool expected)
+        {
+            // Arrange
+            IsPalindrome solution = new IsPalindrome();
+
+            // Act
+            bool result = solution.Solve_v1(x);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(121, true)]
+        [InlineData(-121, false)]
+        [InlineData(10, false)]
+        [InlineData(12321, true)]
+        [InlineData(123456789, false)]
+        public void TestIsPalindrome_v2(int x, bool expected)
+        {
+            // Arrange
+            IsPalindrome solution = new IsPalindrome();
+
+            // Act
+            bool result = solution.Solve_v2(x);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
