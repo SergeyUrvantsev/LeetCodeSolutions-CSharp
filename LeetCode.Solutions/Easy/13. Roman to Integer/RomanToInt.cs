fc7da1f227bbbc412f8a02ed0551e6b661e1d22a
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-06-03 15:17:18 +0300

    13. Roman to Integer

---
+++ LeetCode.Solutions/Easy/13. Roman to Integer/RomanToInt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Easy._13._Roman_to_Integer
+{
+    public class RomanToInt
+    {
+        private static readonly Dictionary<char, int> Map = new()
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        //Complexity
+        //Time: O(n), где n - длина входной строки. Это потому, что мы проходим по каждому символу в строке один раз.
+        //Space: O(1), так как мы используем постоянное количество дополнительной памяти для хранения словаря и переменных.
+
+        public int Solve(string s) =>
+            s.Select((c, i) => i < s.Length - 1 && Map[c] < Map[s[i + 1]] ? -Map[c] : Map[c]).Sum();
+    }
+}
---
+++ LeetCode.Tests/Easy/13. Roman to Integer/RomanToIntTest.cs
@@ -0,0 +1,34 @@
+﻿using LeetCode.Solutions.Easy._13._Roman_to_Integer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Easy._13._Roman_to_Integer
+{
+    public class RomanToIntTest
+    {
+        [Theory]
+        [InlineData("III", 3)]
+        [InlineData("LVIII", 58)]
+        [InlineData("MCMXCIV", 1994)]
+        [InlineData("IX", 9)]
+        [InlineData("XIV", 14)]
+        [InlineData("XL", 40)]
+        [InlineData("XC", 90)]
+        [InlineData("CD", 400)]
+        [InlineData("CM", 900)]
+        public void TestRomanToInt(string s, int expected)
+        {
+            // Arrange
+            RomanToInt solution = new RomanToInt();
+
+            // Act
+            int result = solution.Solve(s);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
