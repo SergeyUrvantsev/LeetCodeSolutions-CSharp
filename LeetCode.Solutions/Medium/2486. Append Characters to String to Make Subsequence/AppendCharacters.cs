2d7e1c00777ba8b513c22017222b437e8b62090b
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-06-03 12:23:55 +0300

    2486. Append Characters to String to Make Subsequence

---
+++ LeetCode.Solutions/Medium/2486. Append Characters to String to Make Subsequence/AppendCharacters.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Medium._2486._Append_Characters_to_String_to_Make_Subsequence
+{
+    public class AppendCharacters
+    {
+        public int Solve(string s, string t)
+        {
+            int sIndex = 0;
+            int tIndex = 0;
+            int maxLengthDiff = t.Length;
+
+            while (sIndex < s.Length)
+            {
+                if (s[sIndex] == t[tIndex])
+                {
+                    if (--maxLengthDiff == 0)
+                        return 0;
+                    tIndex++;
+                }
+                sIndex++;
+            }
+
+            return maxLengthDiff;
+        }
+    }
+}
---
+++ LeetCode.Tests/Medium/2486. Append Characters to String to Make Subsequence/AppendCharactersTest.cs
@@ -0,0 +1,53 @@
+﻿using LeetCode.Solutions.Easy;
+using LeetCode.Solutions.Medium._2486._Append_Characters_to_String_to_Make_Subsequence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Medium._2486._Append_Characters_to_String_to_Make_Subsequence
+{
+    public class AppendCharactersTest
+    {
+        [Fact]
+        public void Solve_ReturnsCorrectIndices_Case1()
+        {
+            var appendCharacters = new AppendCharacters();
+            var result = appendCharacters.Solve("coaching", "coding");
+            Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void Solve_ReturnsCorrectIndices_Case2()
+        {
+            var appendCharacters = new AppendCharacters();
+            var result = appendCharacters.Solve("abcde", "a");
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Solve_ReturnsCorrectIndices_Case3()
+        {
+            var appendCharacters = new AppendCharacters();
+            var result = appendCharacters.Solve("z", "abcde");
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void Solve_ReturnsCorrectIndices_Case4()
+        {
+            var appendCharacters = new AppendCharacters();
+            var result = appendCharacters.Solve("lbg", "g");
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Solve_ReturnsCorrectIndices_Case5()
+        {
+            var appendCharacters = new AppendCharacters();
+            var result = appendCharacters.Solve("vrykt", "rkge");
+            Assert.Equal(2, result);
+        }
+    }
+}
