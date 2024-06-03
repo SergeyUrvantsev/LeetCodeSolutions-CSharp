2bc4eaf924688eff88ba4b7e68fcd0e9b9aa7b65
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-06-03 15:22:50 +0300

    14. Longest Common Prefix

---
+++ LeetCode.Solutions/Easy/14. Longest Common Prefix/LongestCommonPrefix.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Easy._14._Longest_Common_Prefix
+{
+    public class LongestCommonPrefix
+    {
+
+        //Complexity
+        //Time: O(n * m), где n - количество строк в массиве, а m - длина самой короткой строки в массиве.
+        //  Мы проходим по каждому символу короткой строки и сравниваем его с символами в других строках.
+        //Space: O(1), так как мы используем постоянное количество дополнительной
+        //  памяти для хранения переменных.
+
+        public string Solve(string[] strs)
+        {
+            var result = string.Empty;
+            var minLenght = strs.Min(str => str.Length);
+            for (int i = 0; i < minLenght; i++)
+            {
+                for (int j = 0; j < strs.Length - 1; j++)
+                {
+                    if (strs[j][i] != strs[j + 1][i])
+                    {
+                        return result;
+                    }
+                }
+                result += strs[0][i];
+            }
+            return result;
+        }
+    }
+}
---
+++ LeetCode.Tests/Easy/14. Longest Common Prefix/LongestCommonPrefixTest.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.Solutions.Easy._14._Longest_Common_Prefix;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Easy._14._Longest_Common_Prefix
+{
+    public class LongestCommonPrefixTest
+    {
+        [Theory]
+        [InlineData(new string[] { "flower", "flow", "flight" }, "fl")]
+        [InlineData(new string[] { "dog", "racecar", "car" }, "")]
+        [InlineData(new string[] { "apple", "app", "ape" }, "ap")]
+        [InlineData(new string[] { "color", "coast", "come" }, "co")]
+        [InlineData(new string[] { "prefix", "preference", "preschool" }, "pre")]
+        public void TestLongestCommonPrefix(string[] strs, string expected)
+        {
+            // Arrange
+            LongestCommonPrefix solution = new LongestCommonPrefix();
+
+            // Act
+            string result = solution.Solve(strs);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
