11f9dcf89533c8a5c13c764b32f8cc0d7465ff1b
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-06-03 14:22:08 +0300

    3. Longest Substring Without Repeating Characters

---
+++ LeetCode.Solutions/Medium/3. Longest Substring Without Repeating Characters/LengthOfLongestSubstring.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Medium._3._Longest_Substring_Without_Repeating_Characters
+{
+    public class LengthOfLongestSubstring
+    {
+        public int Solve(string s)
+        {
+            int n = s.Length;
+            int maxLength = 0;
+            int left = 0;
+            Dictionary<char, int> charIndexMap = new Dictionary<char, int>();
+
+            for (int right = 0; right < n; right++)
+            {
+                if (charIndexMap.ContainsKey(s[right]) && charIndexMap[s[right]] >= left)
+                {
+                    left = charIndexMap[s[right]] + 1;
+                }
+
+                charIndexMap[s[right]] = right;
+                maxLength = Math.Max(maxLength, right - left + 1);
+            }
+
+            return maxLength;
+        }
+    }
+}
---
+++ LeetCode.Tests/Medium/3. Longest Substring Without Repeating Characters/LengthOfLongestSubstringTest.cs
@@ -0,0 +1,37 @@
+﻿using LeetCode.Solutions.Medium._3._Longest_Substring_Without_Repeating_Characters;
+using LeetCode.Solutions.Medium._6._Zigzag_Conversion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Medium._3._Longest_Substring_Without_Repeating_Characters
+{
+    public class LengthOfLongestSubstringTest
+    {
+        [Fact]
+        public void Solve_ReturnsCorrectIndices_Case1()
+        {
+            var lengthOfLongestSubstring = new LengthOfLongestSubstring();
+            var result = lengthOfLongestSubstring.Solve("abcabcbb");
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void Solve_ReturnsCorrectIndices_Case2()
+        {
+            var lengthOfLongestSubstring = new LengthOfLongestSubstring();
+            var result = lengthOfLongestSubstring.Solve("bbbbb");
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Solve_ReturnsCorrectIndices_Case3()
+        {
+            var lengthOfLongestSubstring = new LengthOfLongestSubstring();
+            var result = lengthOfLongestSubstring.Solve("pwwkew");
+            Assert.Equal(3, result);
+        }
+    }
+}
