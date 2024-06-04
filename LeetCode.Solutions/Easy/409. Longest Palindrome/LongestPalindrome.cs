d174a5c81bbb56263a1cdb9e9b1747cd2a1ba8ff
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-06-04 12:06:47 +0300

    409. Longest Palindrome

---
+++ LeetCode.Solutions/Easy/409. Longest Palindrome/LongestPalindrome.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Easy._409._Longest_Palindrome
+{
+    public class LongestPalindrome
+    {
+        //Complexity
+        //Time: O(n), где n это длина строки s. Алгоритм проходит по строке один раз.
+        //Space: O(1), хотя HashSet может в худшем случае содержать до всех символов строки,
+        //  но количество возможных уникальных символов ограничено
+        //  (256 для ASCII, 65536 для Unicode),
+        //  поэтому пространственная сложность можно считать постоянной.
+
+        public int Solve(string s)
+        {
+            var charSet = new HashSet<char>();
+            var palindromeLength = 0;
+
+            foreach (char c in s)
+            {
+                if (charSet.Contains(c))
+                {
+                    palindromeLength += 2;
+                    charSet.Remove(c);
+                }
+                else
+                {
+                    charSet.Add(c);
+                }
+            }
+
+            return palindromeLength + (charSet.Count > 0 ? 1 : 0);
+        }
+    }
+}
---
+++ LeetCode.Tests/Easy/409. Longest Palindrome/LongestPalindromeTest.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.Solutions.Easy._14._Longest_Common_Prefix;
+using LeetCode.Solutions.Easy._409._Longest_Palindrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Easy._409._Longest_Palindrome
+{
+    public class LongestPalindromeTest
+    {
+        [Theory]
+        [InlineData("abccccdd", 7)]
+        [InlineData("a", 1)]
+        [InlineData("aA", 1)]
+        [InlineData("aaeqfq", 5)]
+        public void TestLongestCommonPrefix(string str, int expected)
+        {
+            // Arrange
+            LongestPalindrome solution = new LongestPalindrome();
+
+            // Act
+            var result = solution.Solve(str);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
