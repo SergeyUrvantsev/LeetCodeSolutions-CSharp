eef244370639171baa95a4c16b8b7958e10582c8
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-06-04 14:15:03 +0300

    8. String to Integer (atoi)

---
+++ LeetCode.Solutions/Medium/8. String to Integer (atoi)/StringToIntegerAtoi.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Medium._8._String_to_Integer__atoi_
+{
+    public class StringToIntegerAtoi
+    {
+
+        //Complexity:
+        //Time complexity: O(n), где n длина строки s
+        //Space complexity: O(1)
+
+        public int Solve(string s)
+        {
+            int res = 0;
+            int i = 0;
+            bool isNegative = false;
+
+            // Skip leading whitespaces
+            while (i < s.Length && s[i] == ' ')
+            {
+                i++;
+            }
+
+            // Handle optional sign
+            if (i < s.Length && (s[i] == '-' || s[i] == '+'))
+            {
+                isNegative = s[i] == '-';
+                i++;
+            }
+
+            // Process digits and avoid overflow
+            while (i < s.Length && char.IsDigit(s[i]))
+            {
+                int digit = s[i] - '0';
+
+                // Check for overflow
+                if (res > (int.MaxValue - digit) / 10)
+                {
+                    return isNegative ? int.MinValue : int.MaxValue;
+                }
+
+                res = res * 10 + digit;
+                i++;
+            }
+
+            return isNegative ? -res : res;
+        }
+    }
+}
---
+++ LeetCode.Tests/Medium/8. String to Integer (atoi)/StringToIntegerAtoiTest.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode.Solutions.Medium._7._Reverse_Integer;
+using LeetCode.Solutions.Medium._8._String_to_Integer__atoi_;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Medium._8._String_to_Integer__atoi_
+{
+    public class StringToIntegerAtoiTest
+    {
+        [Theory]
+        [InlineData("42", 42)]
+        [InlineData(" -042", -42)]
+        [InlineData("1337c0d3", 1337)]
+        [InlineData("0-1", 0)]
+        [InlineData("words and 987", 0)]
+        public void TestStringToIntegerAtoi(string x, int expected)
+        {
+            // Arrange
+            var solution = new StringToIntegerAtoi();
+
+            // Act
+            var result = solution.Solve(x);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
