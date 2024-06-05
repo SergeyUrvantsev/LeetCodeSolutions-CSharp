e65be9930e29faafcb7a9c59cb807832176431b3
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-06-05 15:29:40 +0300

    1002. Find Common Characters

---
+++ LeetCode.Solutions/Easy/1002. Find Common Characters/CommonChars.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace LeetCode.Solutions.Easy._1002._Find_Common_Characters
+{
+    public class CommonChars
+    {
+        //Complexity
+        //Time: O(n), где n это общее количество символов во всех словах
+        //Space: O(1), поскольку используемые массивы фиксированного размера
+
+        private const Byte lettersCount = 26;
+
+        public IList<string> Solve(string[] words)
+        {
+            var charCounts = new Byte[lettersCount];
+            var wordCharCounts = new Byte[lettersCount];
+
+            foreach (char chr in words[0])
+            {
+                charCounts[chr - 'a']++;
+            }
+
+            for (var i = 1; i < words.Length; i++)
+            {
+                foreach (char chr in words[i])
+                {
+                    wordCharCounts[chr - 'a']++;
+                }
+
+                for (var j = Byte.MinValue; j < lettersCount; j++)
+                {
+                    if (wordCharCounts[j] < charCounts[j])
+                        charCounts[j] = wordCharCounts[j];
+                    wordCharCounts[j] = Byte.MinValue;
+                }
+            }
+
+            var result = new List<string>();
+            for (var i = Byte.MinValue; i < lettersCount; i++)
+            {
+                while (!charCounts[i].Equals(Byte.MinValue))
+                {
+                    result.Add(((char)('a' + i)).ToString());
+                    charCounts[i]--;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode.Tests/Easy/1002. Find Common Characters/CommonCharsTest.cs
@@ -0,0 +1,38 @@
+﻿using LeetCode.Solutions.Easy._1002._Find_Common_Characters;
+using LeetCode.Solutions.Easy._9._Palindrome_Number;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Easy._1002._Find_Common_Characters
+{
+    public class CommonCharsTest
+    {
+        [Theory]
+        [InlineData(new string[] { "bella", "label", "roller" }, new string[] { "e", "l", "l" })]
+        [InlineData(new string[] { "cool", "lock", "cook" }, new string[] { "c", "o" })]
+        public void TestCommonChars(string[] words, string[] expected)
+        {
+            // Arrange
+            var solution = new CommonChars();
+
+            // Act
+            var result = solution.Solve(words);
+
+            // Assert
+            Assert.Equal(expected.Length, result.Count);
+
+            AssertArrayContainsElements(expected, expected);
+        }
+
+        private void AssertArrayContainsElements(IList<string> actual, string[] expected)
+        {
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.Equal(expected[i], actual[i]);
+            }
+        }
+    }
+}
