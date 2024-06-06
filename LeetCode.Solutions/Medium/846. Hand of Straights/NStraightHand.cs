8be789d74029b2b42dd7bb3da166729bd5aa3fe6
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-06-06 15:04:48 +0300

    846. Hand of Straights

---
+++ LeetCode.Solutions/Medium/846. Hand of Straights/NStraightHand.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Medium._846._Hand_of_Straights
+{
+    public class NStraightHand
+    {
+        // Complexity:
+        // Time complexity: O(n log n), где n - количество карт в руке (hand.Length)
+        // Space complexity: O(n), для хранения карты и их счетчиков
+
+        public bool Solve_v1(int[] hand, int groupSize)
+        {
+            if (hand.Length % groupSize != 0)
+                return false;
+
+            var groups = new int[hand.Length / groupSize, 2];
+
+            for (int i = 0; i < hand.Length / groupSize; i++)
+            {
+                groups[i, 0] = 0;
+                groups[i, 1] = -1;
+            }
+
+            foreach (var card in hand.Order())
+            {
+                var pos = -1;
+
+                for (int i = 0; i < hand.Length / groupSize; i++)
+                {
+                    if (groups[i, 0] < groupSize
+                        && (card - groups[i, 1] == 1 || groups[i, 1] == -1))
+                    {
+                        pos = i;
+                        break;
+                    }
+                }
+
+                if (pos == -1)
+                    return false;
+
+                groups[pos, 0]++;
+                groups[pos, 1] = card;
+            }
+
+            return true;
+        }
+
+        public bool Solve_v2(int[] hand, int groupSize)
+        {
+            if (hand.Length % groupSize != 0)
+                return false;
+
+            // Считаем количество каждой карты
+            var cardCount = new Dictionary<int, int>();
+            foreach (var card in hand)
+            {
+                if (cardCount.ContainsKey(card))
+                    cardCount[card]++;
+                else
+                    cardCount[card] = 1;
+            }
+
+            // Получаем все уникальные карты и сортируем их
+            var uniqueCards = new List<int>(cardCount.Keys);
+            uniqueCards.Sort();
+
+            foreach (var card in uniqueCards)
+            {
+                int count = cardCount[card];
+                if (count > 0)
+                {
+                    for (int i = 0; i < groupSize; i++)
+                    {
+                        int currentCard = card + i;
+                        if (!cardCount.ContainsKey(currentCard) || cardCount[currentCard] < count)
+                            return false;
+
+                        cardCount[currentCard] -= count;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public bool Solve_v3(int[] hand, int groupSize)
+        {
+            if (hand.Length % groupSize != 0)
+                return false;
+
+            var cardCount = new Dictionary<int, int>();
+            foreach (var card in hand)
+            {
+                cardCount[card] = cardCount.GetValueOrDefault(card, 0) + 1;
+            }
+
+            var uniqueCards = new List<int>(cardCount.Keys);
+            uniqueCards.Sort();
+
+            foreach (var card in uniqueCards)
+            {
+                if (cardCount[card] > 0)
+                {
+                    var count = cardCount[card];
+                    for (int currentCard = card; currentCard < card + groupSize; currentCard++)
+                    {
+                        if (!cardCount.ContainsKey(currentCard) || cardCount[currentCard] < count)
+                            return false;
+
+                        cardCount[currentCard] -= count;
+                    }
+                }
+            }
+
+            GC.Collect();
+            return true;
+        }
+    }
+}
---
+++ LeetCode.Tests/Medium/846. Hand of Straights/NStraightHandTests.cs
@@ -0,0 +1,70 @@
+﻿using LeetCode.Solutions.Easy._9._Palindrome_Number;
+using LeetCode.Solutions.Medium._846._Hand_of_Straights;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Medium._846._Hand_of_Straights
+{
+    public class NStraightHandTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 6, 2, 3, 4, 7, 8 }, 3, true)]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, 4, false)]
+        [InlineData(new int[] { 8, 10, 12 }, 3, false)]
+        [InlineData(new int[] { 2, 3, 4, 5, 6, 7, 8, 9 }, 8, true)]
+        [InlineData(new int[] { 8, 8, 9, 7, 7, 7, 6, 7, 10, 6 }, 2, true)]
+        [InlineData(new int[] { 9, 13, 15, 23, 22, 25, 4, 4, 29, 15, 8, 23, 12, 19, 24, 17, 18, 11, 22, 24, 17, 17, 10, 23, 21, 18, 14, 18, 7, 6, 3, 6, 19, 11, 16, 11, 12, 13, 8, 26, 17, 20, 13, 19, 22, 21, 27, 9, 20, 15, 20, 27, 8, 13, 25, 23, 22, 15, 9, 14, 20, 10, 6, 5, 14, 12, 7, 16, 21, 18, 21, 24, 23, 10, 21, 16, 18, 16, 18, 5, 20, 19, 20, 10, 14, 26, 2, 9, 19, 12, 28, 17, 5, 7, 25, 22, 16, 17, 21, 11 }, 10, false)]
+        public void TestNStraightHand_v1(int[] hand, int groupSize, bool expected)
+        {
+            // Arrange
+            var solution = new NStraightHand();
+
+            // Act
+            bool result = solution.Solve_v1(hand, groupSize);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 6, 2, 3, 4, 7, 8 }, 3, true)]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, 4, false)]
+        [InlineData(new int[] { 8, 10, 12 }, 3, false)]
+        [InlineData(new int[] { 2, 3, 4, 5, 6, 7, 8, 9 }, 8, true)]
+        [InlineData(new int[] { 8, 8, 9, 7, 7, 7, 6, 7, 10, 6 }, 2, true)]
+        [InlineData(new int[] { 9, 13, 15, 23, 22, 25, 4, 4, 29, 15, 8, 23, 12, 19, 24, 17, 18, 11, 22, 24, 17, 17, 10, 23, 21, 18, 14, 18, 7, 6, 3, 6, 19, 11, 16, 11, 12, 13, 8, 26, 17, 20, 13, 19, 22, 21, 27, 9, 20, 15, 20, 27, 8, 13, 25, 23, 22, 15, 9, 14, 20, 10, 6, 5, 14, 12, 7, 16, 21, 18, 21, 24, 23, 10, 21, 16, 18, 16, 18, 5, 20, 19, 20, 10, 14, 26, 2, 9, 19, 12, 28, 17, 5, 7, 25, 22, 16, 17, 21, 11 }, 10, false)]
+        public void TestNStraightHand_v2(int[] hand, int groupSize, bool expected)
+        {
+            // Arrange
+            var solution = new NStraightHand();
+
+            // Act
+            bool result = solution.Solve_v2(hand, groupSize);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 6, 2, 3, 4, 7, 8 }, 3, true)]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, 4, false)]
+        [InlineData(new int[] { 8, 10, 12 }, 3, false)]
+        [InlineData(new int[] { 2, 3, 4, 5, 6, 7, 8, 9 }, 8, true)]
+        [InlineData(new int[] { 8, 8, 9, 7, 7, 7, 6, 7, 10, 6 }, 2, true)]
+        [InlineData(new int[] { 9, 13, 15, 23, 22, 25, 4, 4, 29, 15, 8, 23, 12, 19, 24, 17, 18, 11, 22, 24, 17, 17, 10, 23, 21, 18, 14, 18, 7, 6, 3, 6, 19, 11, 16, 11, 12, 13, 8, 26, 17, 20, 13, 19, 22, 21, 27, 9, 20, 15, 20, 27, 8, 13, 25, 23, 22, 15, 9, 14, 20, 10, 6, 5, 14, 12, 7, 16, 21, 18, 21, 24, 23, 10, 21, 16, 18, 16, 18, 5, 20, 19, 20, 10, 14, 26, 2, 9, 19, 12, 28, 17, 5, 7, 25, 22, 16, 17, 21, 11 }, 10, false)]
+        public void TestNStraightHand_v3(int[] hand, int groupSize, bool expected)
+        {
+            // Arrange
+            var solution = new NStraightHand();
+
+            // Act
+            bool result = solution.Solve_v3(hand, groupSize);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
