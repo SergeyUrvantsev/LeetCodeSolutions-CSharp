bfd696338b14c9d8eb97532e1c654564bbd86bae
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-06-07 11:50:53 +0300

    648. Replace Words

---
+++ LeetCode.Solutions/Medium/648. Replace Words/ReplaceWords.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Medium._648._Replace_Words
+{
+    public class ReplaceWords
+    {
+        //Решение нацелено на оптимизацию использования памяти, а не времени выполнения
+        //Для оптимизации времени выполнения рекомендуется использовать деревья префиксов
+
+        //Complexity:
+        //Time complexity: O(n * m + k * log(k)), где n - количество слов в предложении, 
+        //m - средняя длина слова, k - количество слов в словаре.
+        //Space complexity: O(k), где k - количество слов в словаре.
+
+        public string Solve(IList<string> dictionary, string sentence)
+        {
+            var roots = dictionary.ToList();
+            roots.Sort();
+
+            return string.Join(" ", sentence.Split(' ')
+                .Select(word => roots.Find(root => word.StartsWith(root)) ?? word));
+        }
+    }
+}
---
+++ LeetCode.Tests/Medium/648. Replace Words/ReplaceWordsTests.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Solutions.Medium._648._Replace_Words;
+using LeetCode.Solutions.Medium._846._Hand_of_Straights;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Medium._648._Replace_Words
+{
+    public class ReplaceWordsTests
+    {
+        [Theory]
+        [InlineData(new string[]{ "cat", "bat", "rat" }, "the cattle was rattled by the battery", "the cat was rat by the bat")]
+        [InlineData(new string[] { "a", "b", "c" }, "aadsfasf absbs bbab cadsfafs", "a a b c")]
+        [InlineData(new string[] { "catt", "cat", "bat", "rat" }, "the cattle was rattled by the battery", "the cat was rat by the bat")]
+        public void TestReplaceWords(string[] dictionary, string sentence, string expected)
+        {
+            // Arrange
+            var solution = new ReplaceWords();
+
+            // Act
+            var result = solution.Solve(dictionary.ToList(), sentence);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
