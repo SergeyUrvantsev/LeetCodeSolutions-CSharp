27dc7ca69b048fe490e1390ee427f2acd7eaf540
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-06-10 11:43:30 +0300

    1051. Height Checker

---
+++ LeetCode.Solutions/Easy/1051. Height Checker/HeightChecker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Easy._1051._Height_Checker
+{
+    public class HeightChecker
+    {
+        // Complexity:
+        // Time complexity: O(n), где n - количество элементов в массиве heights.
+        // Space complexity: O(1), так как максимальное количество различных значений фиксировано и ограничено.
+
+        public int Solve(int[] heights)
+        {
+            int[] heightCounts = new int[101];
+            foreach (var height in heights)
+            {
+                heightCounts[height]++;
+            }
+
+            int result = 0, currentHeight = 0;
+            foreach (var height in heights)
+            {
+                while (heightCounts[currentHeight] == 0)
+                {
+                    currentHeight++;
+                }
+
+                if (height != currentHeight)
+                {
+                    result++;
+                }
+
+                heightCounts[currentHeight]--;
+            }
+
+            GC.Collect();
+            return result;
+        }
+    }
+}
---
+++ LeetCode.Tests/Easy/1051. Height Checker/HeightCheckerTest.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Solutions.Easy._1051._Height_Checker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Easy._1051._Height_Checker
+{
+    public class HeightCheckerTest
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 1, 4, 2, 1, 3 }, 3)]
+        [InlineData(new int[] { 5, 1, 2, 3, 4 }, 5)]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, 0)]
+        public void TestLongestPalindrome(int[] heights, int expected)
+        {
+            // Arrange
+            var solution = new HeightChecker();
+
+            // Act
+            var result = solution.Solve(heights);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
