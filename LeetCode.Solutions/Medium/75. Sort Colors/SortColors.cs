1c8032ebf925d7f52615fe7cd6c57cd18723a4d5
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-06-12 13:06:15 +0300

    75. Sort Colors

---
+++ LeetCode.Solutions/Medium/75. Sort Colors/SortColors.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Medium._75._Sort_Colors
+{
+    public class SortColors
+    {
+        //Complexity:
+        //Time complexity: O(n), где n это длина массива nums
+        //Space complexity: O(1)
+
+        public void Solve(int[] nums)
+        {
+            int count0 = 0, count1 = 0, count2 = 0;
+
+            foreach (int num in nums)
+            {
+                switch (num)
+                {
+                    case 0:
+                        count0++;
+                        break;
+                    case 1:
+                        count1++;
+                        break;
+                    case 2:
+                        count2++;
+                        break;
+                }
+            }
+
+            int index = 0;
+            for (int i = 0; i < count0; i++)
+            {
+                nums[index++] = 0;
+            }
+            for (int i = 0; i < count1; i++)
+            {
+                nums[index++] = 1;
+            }
+            for (int i = 0; i < count2; i++)
+            {
+                nums[index++] = 2;
+            }
+        }
+    }
+}
---
+++ LeetCode.Tests/Medium/75. Sort Colors/SortColorsTests.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode.Solutions.Medium._75._Sort_Colors;
+using LeetCode.Solutions.Medium._846._Hand_of_Straights;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Medium._75._Sort_Colors
+{
+    public class SortColorsTests
+    {
+        [Theory]
+        [InlineData(new int[] { 2, 0, 2, 1, 1, 0 }, new int[] { 0, 0, 1, 1, 2, 2 })]
+        [InlineData(new int[] { 2, 0, 1 }, new int[] { 0, 1, 2 })]
+        [InlineData(new int[] { 0 }, new int[] { 0 })]
+        [InlineData(new int[] { 1 }, new int[] { 1 })]
+        [InlineData(new int[] { 2, 2, 1, 1, 0, 0 }, new int[] { 0, 0, 1, 1, 2, 2 })]
+        public void TestSortColors(int[] input, int[] expected)
+        {
+            // Arrange
+            var solution = new SortColors();
+
+            // Act
+            solution.Solve(input);
+
+            // Assert
+            Assert.Equal(expected, input);
+        }
+    }
+}
