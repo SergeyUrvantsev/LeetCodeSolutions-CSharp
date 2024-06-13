b94cdcdaf1fb2d40a46c9d25cfc91ae08e78f5b9
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-06-13 10:54:58 +0300

    2037. Minimum Number of Moves to Seat Everyone

---
+++ LeetCode.Solutions/Easy/2037. Minimum Number of Moves to Seat Everyone/MinMovesToSeat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Easy._2037._Minimum_Number_of_Moves_to_Seat_Everyone
+{
+    public class MinMovesToSeat
+    {
+        // Complexity:
+        // Time: O(n log n), где n - количество элементов в массивах seats и students
+        // Space: O(1), так как сортировка происходит на месте и используется только несколько дополнительных переменных
+
+        public int Solve(int[] seats, int[] students)
+        {
+            Array.Sort(seats);
+            Array.Sort(students);
+
+            var moves = 0;
+
+            for (int i = 0; i < seats.Length; i++)
+            {
+                moves += Math.Abs(seats[i] - students[i]);
+            }
+
+            return moves;
+        }
+    }
+}
---
+++ LeetCode.Tests/Easy/2037. Minimum Number of Moves to Seat Everyone/MinMovesToSeatTests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Solutions.Easy._2037._Minimum_Number_of_Moves_to_Seat_Everyone;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Easy._2037._Minimum_Number_of_Moves_to_Seat_Everyone
+{
+    public class MinMovesToSeatTests
+    {
+        [Theory]
+        [InlineData(new int[] { 3, 1, 5 }, new int[] { 2, 7, 4 }, 4)]
+        [InlineData(new int[] { 4, 1, 5, 9 }, new int[] { 1, 3, 2, 6 }, 7)]
+        [InlineData(new int[] { 2, 2, 6, 6 }, new int[] { 1, 3, 2, 6 }, 4)]
+        public void TestMinMovesToSeat(int[] seats, int[] students, int expected)
+        {
+            // Arrange
+            var solution = new MinMovesToSeat();
+
+            // Act
+            var result = solution.Solve(seats, students);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
