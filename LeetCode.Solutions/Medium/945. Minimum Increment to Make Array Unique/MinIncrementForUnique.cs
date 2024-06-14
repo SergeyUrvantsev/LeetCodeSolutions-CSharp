ea67af8a5021acd186254e505cee4151ff75dcce
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-06-14 11:29:41 +0300

    945. Minimum Increment to Make Array Unique

---
+++ LeetCode.Solutions/Medium/945. Minimum Increment to Make Array Unique/MinIncrementForUnique.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Medium._945._Minimum_Increment_to_Make_Array_Unique
+{
+    public class MinIncrementForUnique
+    {
+        public int Solve(int[] nums)
+        {
+            Array.Sort(nums);
+
+            var moves = 0;
+            var maxNum = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] <= maxNum)
+                {
+                    moves += maxNum - nums[i] + 1;
+                    maxNum += 1;
+                }
+                else
+                {
+                    maxNum = nums[i];
+                }
+                
+            }
+
+            return moves;
+        }
+    }
+}
---
+++ LeetCode.Tests/Medium/945. Minimum Increment to Make Array Unique/MinIncrementForUniqueTests.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Solutions.Medium._846._Hand_of_Straights;
+using LeetCode.Solutions.Medium._945._Minimum_Increment_to_Make_Array_Unique;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Medium._945._Minimum_Increment_to_Make_Array_Unique
+{
+    public class MinIncrementForUniqueTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 2 }, 1)]
+        [InlineData(new int[] { 3, 2, 1, 2, 1, 7 }, 6)]
+        [InlineData(new int[] { 3, 3, 0, 2, 0, 9, 2, 11, 10, 14, 5, 13, 6, 5, 1 }, 21)]
+        public void TestMinIncrementForUnique(int[] nums, int expected)
+        {
+            // Arrange
+            var solution = new MinIncrementForUnique();
+
+            // Act
+            var result = solution.Solve(nums);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
