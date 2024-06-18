88349cdf1e00e48619825c8c1056a7d6dfb23e19
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-06-18 16:48:06 +0300

    273. Integer to English Words

---
+++ LeetCode.Solutions/Hard/273. Integer to English Words/NumberToWords.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Hard._273._Integer_to_English_Words
+{
+    public class NumberToWords
+    {
+        private static readonly string[] _lessThanTwenty = { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+        private static readonly string[] _tens = { "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+        private static readonly string[] _thousands = { "", "Thousand", "Million", "Billion" };
+
+        public string Solve(int num)
+        {
+            if (num == 0) return "Zero";
+
+            int i = 0;
+            var words = new StringBuilder();
+
+            while (num > 0)
+            {
+                if (num % 1000 != 0)
+                {
+                    var segment = ConvertThreeDigits(num % 1000);
+                    words.Insert(0, segment + _thousands[i] + " ");
+                }
+                num /= 1000;
+                i++;
+            }
+
+            return words.ToString().Trim();
+        }
+
+        private string ConvertThreeDigits(int num)
+        {
+            var result = new StringBuilder();
+
+            if (num >= 100)
+            {
+                result.Append(_lessThanTwenty[num / 100] + " Hundred ");
+                num %= 100;
+            }
+
+            if (num >= 20)
+            {
+                result.Append(_tens[num / 10] + " ");
+                num %= 10;
+            }
+
+            if (num > 0)
+            {
+                result.Append(_lessThanTwenty[num] + " ");
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ LeetCode.Tests/Hard/273. Integer to English Words/NumberToWordsTests.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode.Solutions.Hard._273._Integer_to_English_Words;
+using LeetCode.Solutions.Hard._502._IPO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Hard._273._Integer_to_English_Words
+{
+    public class NumberToWordsTests
+    {
+        [Theory]
+        [InlineData(123, "One Hundred Twenty Three")]
+        [InlineData(12345, "Twelve Thousand Three Hundred Forty Five")]
+        [InlineData(1234567, "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven")]
+        [InlineData(1000010, "One Million Ten")]
+        [InlineData(0, "Zero")]
+        public void TestNumberToWords(int num, string expected)
+        {
+            // Arrange
+            var solution = new NumberToWords();
+
+            // Act
+            var result = solution.Solve(num);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
