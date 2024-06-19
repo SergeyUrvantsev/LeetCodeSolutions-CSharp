99413d227fb7a85409fbb0f7a295dd5796217035
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-06-19 14:46:16 +0300

    1482. Minimum Number of Days to Make m Bouquets

---
+++ LeetCode.Solutions/Medium/1482. Minimum Number of Days to Make m Bouquets/MinDays.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Medium._1482._Minimum_Number_of_Days_to_Make_m_Bouquets
+{
+    public class MinDays
+    {
+
+        //Complexity:
+        //Time complexity: O(n log D), где n это длина bloomDay, D это диапозон дней (max - min). 
+        //Space complexity: O(1)
+
+        public int Solve(int[] bloomDay, int m, int k)
+        {
+            if ((long)m * k > bloomDay.Length)
+            {
+                return -1;
+            }
+
+            int left = 1, right = bloomDay.Max();
+
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                if (CanMakeBouquets(mid))
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            return left;
+
+            bool CanMakeBouquets(int day)
+            {
+                int bouquets = 0, flowers = 0;
+
+                foreach (var bloom in bloomDay)
+                {
+                    if (bloom <= day)
+                    {
+                        flowers++;
+                        if (flowers == k)
+                        {
+                            bouquets++;
+                            flowers = 0;
+                        }
+                    }
+                    else
+                    {
+                        flowers = 0;
+                    }
+
+                    if (bouquets >= m)
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        }
+    }
+}
---
+++ LeetCode.Tests/Medium/1482. Minimum Number of Days to Make m Bouquets/MinDaysTests.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.Solutions.Medium._1482._Minimum_Number_of_Days_to_Make_m_Bouquets;
+using LeetCode.Solutions.Medium._8._String_to_Integer__atoi_;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Medium._1482._Minimum_Number_of_Days_to_Make_m_Bouquets
+{
+    public class MinDaysTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 10, 3, 10, 2 }, 3, 1, 3)]
+        [InlineData(new int[] { 1, 10, 3, 10, 2 }, 3, 2, -1)]
+        [InlineData(new int[] { 7, 7, 7, 7, 12, 7, 7 }, 2, 3, 12)]
+        [InlineData(new int[] { 1, 10, 2, 9, 3, 8, 4, 7, 5, 6 }, 4, 2, 9)]
+        public void TestMinDays(int[] bloomDay, int m, int k, int expected)
+        {
+            // Arrange
+            var solution = new MinDays();
+
+            // Act
+            var result = solution.Solve(bloomDay, m, k);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
