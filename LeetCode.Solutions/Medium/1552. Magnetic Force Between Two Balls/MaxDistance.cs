d3765de1d10170f2313ddb44cea75441afbb512d
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-06-20 15:32:41 +0300

    1552. Magnetic Force Between Two Balls

---
+++ LeetCode.Solutions/Medium/1552. Magnetic Force Between Two Balls/MaxDistance.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Medium._1552._Magnetic_Force_Between_Two_Balls
+{
+    public class MaxDistance
+    {
+        //Complexity 
+        //Time: O(n log m), где n - длина positions, m - разница между максимальной и минимальной позициями в positions
+        //Space: O(1)
+
+        public int Solve(int[] positions, int m)
+        {
+            // O(n log n)
+            Array.Sort(positions);
+
+            int left = 1, mid, right = positions[^1];
+
+
+            // O(n log m)
+            while (left <= right)
+            {
+                mid = (left + right) / 2;
+
+                if (CanPlaceBalls(mid))
+                {
+                    left = mid + 1; 
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return right;
+
+            // O(n)
+            bool CanPlaceBalls(int minDist)
+            {
+                int count = 1, lastPosition = positions[0];
+
+                for (int i = 0; i < positions.Length; i++)
+                {
+                    if (positions[i] - lastPosition >= minDist)
+                    {
+                        count++;
+                        lastPosition = positions[i];
+
+                        if (count >= m)
+                        {
+                            return true;
+                        }
+                    }
+                }
+
+                return false;
+            }
+        }
+    }
+}
---
+++ LeetCode.Tests/Medium/1552. Magnetic Force Between Two Balls/MaxDistanceTests.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Solutions.Medium._1482._Minimum_Number_of_Days_to_Make_m_Bouquets;
+using LeetCode.Solutions.Medium._1552._Magnetic_Force_Between_Two_Balls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Medium._1552._Magnetic_Force_Between_Two_Balls
+{
+    public class MaxDistanceTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 4, 7 }, 3, 3)]
+        [InlineData(new int[] { 5, 4, 3, 2, 1, 1000000000 }, 2, 999999999)]
+        [InlineData(new int[] { 2411, 1459, 1933, 1607, 2, 79, 857, 191, 1009, 311, 2609, 1759, 2089, 2269, 2741, 439, 1297, 1151, 577, 709 }, 11, 162)]
+        public void TestMaxDistance(int[] position, int m, int expected)
+        {
+            // Arrange
+            var solution = new MaxDistance();
+
+            // Act
+            var result = solution.Solve(position, m);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
