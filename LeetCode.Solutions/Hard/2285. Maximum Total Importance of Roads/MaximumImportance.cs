fae1d5192ad41132d363f66bdb2d178e07c63ee9
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-06-28 11:25:45 +0300

    2285. Maximum Total Importance of Roads

---
+++ LeetCode.Solutions/Hard/2285. Maximum Total Importance of Roads/MaximumImportance.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Hard._2285._Maximum_Total_Importance_of_Roads
+{
+    public class MaximumImportance
+    {
+        //Complexity
+        //Time: O(m + n log n), где n - количество городов имеющих хотя бы 1 дорогу, m - количество дорог
+        //Space: O(n), где n - количество городов имеющих хотя бы 1 дорогу
+
+        public long Solve(int n, int[][] roads)
+        {
+            //Space: O(n)
+            var roadsMap = new Dictionary<int, int>();
+
+            //Time: O(m) - проход по всем дорогам
+            foreach (var road in roads)
+            {
+                roadsMap[road[0]] = roadsMap.GetValueOrDefault(road[0], 0) + 1;
+                roadsMap[road[1]] = roadsMap.GetValueOrDefault(road[1], 0) + 1;
+            }
+
+
+            var value = n - roadsMap.Count + 1;
+
+            //Space: O(n)
+            //Time: O(n log n) - сортировка, O(n log n) - преобразование обратно в словарь 
+            roadsMap = roadsMap.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => value++);
+
+            var result = 0l;
+
+            //Time: O(m) - проход по всем дорогам
+            foreach (var road in roads)
+            {
+                result += roadsMap[road[0]] + roadsMap[road[1]];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode.Tests/Hard/2285. Maximum Total Importance of Roads/MaximumImportanceTests.cs
@@ -0,0 +1,66 @@
+﻿using LeetCode.Solutions.Hard._1713._Minimum_Operations_to_Make_a_Subsequence;
+using LeetCode.Solutions.Hard._2285._Maximum_Total_Importance_of_Roads;
+using LeetCode.Solutions.Hard._273._Integer_to_English_Words;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Hard._2285._Maximum_Total_Importance_of_Roads
+{
+    public class MaximumImportanceTests
+    {
+        [Fact]
+        public void Solve_Example_1()
+        {
+            // Arrange
+            var solution = new MaximumImportance();
+            var roads = new int[6][];
+            roads[0] = new int[] { 0, 1 };
+            roads[1] = new int[] { 1, 2 };
+            roads[2] = new int[] { 2, 3 };
+            roads[3] = new int[] { 0, 2 };
+            roads[4] = new int[] { 1, 3 };
+            roads[5] = new int[] { 2, 4 };
+
+            // Act
+            var result = solution.Solve(5, roads);
+
+            // Assert
+            Assert.Equal(43, result);
+        }
+
+        [Fact]
+        public void Solve_Example_2()
+        {
+            // Arrange
+            var solution = new MaximumImportance();
+            var roads = new int[3][];
+            roads[0] = new int[] { 0, 3 };
+            roads[1] = new int[] { 2, 4 };
+            roads[2] = new int[] { 1, 3 };
+
+            // Act
+            var result = solution.Solve(5, roads);
+
+            // Assert
+            Assert.Equal(20, result);
+        }
+
+        [Fact]
+        public void Solve_Example_3()
+        {
+            // Arrange
+            var solution = new MaximumImportance();
+            var roads = new int[1][];
+            roads[0] = new int[] { 0, 1 };
+
+            // Act
+            var result = solution.Solve(5, roads);
+
+            // Assert
+            Assert.Equal(9, result);
+        }
+    }
+}
