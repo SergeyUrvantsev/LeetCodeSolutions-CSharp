4f794bc6b6225135aae96d1b70bf9799c42af4ea
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-07-01 10:48:46 +0300

    1550. Three Consecutive Odds

---
+++ LeetCode.Solutions/Easy/1550. Three Consecutive Odds/ThreeConsecutiveOdds.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Easy._1550._Three_Consecutive_Odds
+{
+    public class ThreeConsecutiveOdds
+    {
+        //Complexity
+        //Time: O(n * m), где n - длинна массива arr
+        //Space: O(1)
+
+        public bool Solve(int[] arr)
+        {
+            var index = 0;
+            foreach (var item in arr)
+            {
+                if(item % 2 == 1)
+                {
+                    index++;
+                }
+                else
+                {
+                    index = 0;
+                }
+
+                if(index == 3)
+                {
+                    return true;
+                }
+                    
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCode.Tests/Easy/1550. Three Consecutive Odds/ThreeConsecutiveOddsTests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Solutions.Easy._1051._Height_Checker;
+using LeetCode.Solutions.Easy._1550._Three_Consecutive_Odds;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Easy._1550._Three_Consecutive_Odds
+{
+    public class ThreeConsecutiveOddsTests
+    {
+        [Theory]
+        [InlineData(new int[] { 2, 6, 4, 1 }, false)]
+        [InlineData(new int[] { 1, 2, 34, 3, 4, 5, 7, 23, 12 }, true)]
+        public void TestThreeConsecutiveOdds(int[] heights, bool expected)
+        {
+            // Arrange
+            var solution = new ThreeConsecutiveOdds();
+
+            // Act
+            var result = solution.Solve(heights);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
