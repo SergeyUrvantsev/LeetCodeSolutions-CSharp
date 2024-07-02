88641ef26aebc1c3492788da19f940c49bb6c739
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-07-02 19:32:47 +0300

    350. Intersection of Two Arrays II

---
+++ LeetCode.Solutions/Easy/350. Intersection of Two Arrays II/Intersect.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Easy._350._Intersection_of_Two_Arrays_II
+{
+    public class Intersect
+    {
+        //Complexity
+        //Time: O(n + m), где n и m — длины массивов nums1 и nums2
+        //Space: O(min(n, m)), так как размер карты и результирующего массива определяется меньшим из массивов
+
+        public int[] Solve(int[] nums1, int[] nums2)
+        {
+            return nums1.Length > nums2.Length
+                ? GetIntersection(nums1, nums2)
+                : GetIntersection(nums2, nums1);
+
+            int[] GetIntersection(int[] larger, int[] smaller)
+            {
+                var map = new Dictionary<int, int>();
+                foreach (var num in larger)
+                {
+                    if (map.ContainsKey(num))
+                    {
+                        map[num]++;
+                    }
+                    else
+                    {
+                        map[num] = 1;
+                    }
+                }
+
+                var intersection = new List<int>();
+
+                foreach (var num in smaller)
+                {
+                    if (map.ContainsKey(num) && map[num] > 0)
+                    {
+                        intersection.Add(num);
+                        map[num]--;
+
+                        if (map[num] == 0)
+                        {
+                            map.Remove(num);
+                        }
+                    }
+                }
+
+                return intersection.ToArray();
+            }
+        }
+    }
+}
---
+++ LeetCode.Tests/Easy/350. Intersection of Two Arrays II/IntersectTests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Solutions.Easy._1550._Three_Consecutive_Odds;
+using LeetCode.Solutions.Easy._350._Intersection_of_Two_Arrays_II;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Easy._350._Intersection_of_Two_Arrays_II
+{
+    public class IntersectTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 2, 1 }, new int[] { 2, 2 }, new int[] { 2, 2 })]
+        [InlineData(new int[] { 4, 9, 5 }, new int[] { 9, 4, 9, 8, 4 }, new int[] { 4, 9 })]
+        public void TestIntersect(int[] nums1, int[] nums2, int[] expected)
+        {
+            // Arrange
+            var solution = new Intersect();
+
+            // Act
+            var result = solution.Solve(nums1, nums2);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
