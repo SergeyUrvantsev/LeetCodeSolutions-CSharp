ec60004ca5cc437299da75bc18fc5e452ba64dca
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-07-03 13:57:15 +0300

    28. Find the Index of the First Occurrence in a String

---
+++ LeetCode.Solutions/Easy/28. Find the Index of the First Occurrence in a String/StrStr.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Easy._28._Find_the_Index_of_the_First_Occurrence_in_a_String
+{
+    public class StrStr
+    {
+        //Complexity
+        //Time: O(n + m), где n и m длинна строк haystack и needle
+        //Space: O(1)
+        public int Solve(string haystack, string needle)
+            => haystack.IndexOf(needle);
+
+        //Complexity
+        //Time: O(nm), где n и m длинна строк haystack и needle
+        //Space: O(1)
+        public int Solve_2(string haystack, string needle)
+        {
+            var index = 0;
+
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                if (haystack[i] != needle[index])
+                {
+                    i -= index;
+                    index = 0;
+                }
+                else if (++index == needle.Length)
+                {
+                    return i - index + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ LeetCode.Tests/Easy/28. Find the Index of the First Occurrence in a String/StrStrTests.cs
@@ -0,0 +1,47 @@
+﻿using LeetCode.Solutions.Easy._27._Remove_Element;
+using LeetCode.Solutions.Easy._28._Find_the_Index_of_the_First_Occurrence_in_a_String;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Easy._28._Find_the_Index_of_the_First_Occurrence_in_a_String
+{
+    public class StrStrTests
+    {
+        [Theory]
+        [InlineData("sadbutsad", "sad", 0)]
+        [InlineData("leetcode", "leeto", -1)]
+        [InlineData("sadleetcode", "lee", 3)]
+        [InlineData("mississippi", "issip", 4)]
+        public void TestStrStr(string haystack, string needle, int expected)
+        {
+            // Arrange
+            var solution = new StrStr();
+
+            // Act
+            var result = solution.Solve(haystack, needle);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("sadbutsad", "sad", 0)]
+        [InlineData("leetcode", "leeto", -1)]
+        [InlineData("sadleetcode", "lee", 3)]
+        [InlineData("mississippi", "issip", 4)]
+        public void TestStrStr_2(string haystack, string needle, int expected)
+        {
+            // Arrange
+            var solution = new StrStr();
+
+            // Act
+            var result = solution.Solve_2(haystack, needle);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
