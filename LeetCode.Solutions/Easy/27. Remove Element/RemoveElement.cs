dcefe1df90cd4e76497ed765efc79883241a3e56
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-07-03 13:28:59 +0300

    27. Remove Element

---
+++ LeetCode.Solutions/Easy/27. Remove Element/RemoveElement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Easy._27._Remove_Element
+{
+    public class RemoveElement
+    {
+        //Complexity
+        //Time: O(n), где n длинна массива nums
+        //Space: O(1)
+
+        public int Solve(int[] nums, int val)
+        {
+            var index = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == val)
+                {
+                    continue;
+                }
+
+                if (index != i)
+                {
+                    nums[index] = nums[i];
+                }
+
+                index++;
+            }
+
+            return index;
+        }
+    }
+}
---
+++ LeetCode.Tests/Easy/27. Remove Element/RemoveElementTests.cs
@@ -0,0 +1,38 @@
+﻿using LeetCode.Solutions.Easy._1002._Find_Common_Characters;
+using LeetCode.Solutions.Easy._27._Remove_Element;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Easy._27._Remove_Element
+{
+    public class RemoveElementTests
+    {
+        [Theory]
+        [InlineData(new int[] { 3, 2, 2, 3 }, 3, new int[] { 2, 2 })]
+        [InlineData(new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2, new int[] { 0, 1, 3, 0, 4 })]
+        public void TestRemoveElement(int[] nums, int val, int[] expected)
+        {
+            // Arrange
+            var solution = new RemoveElement();
+
+            // Act
+            var result = solution.Solve(nums, val);
+
+            // Assert
+            Assert.Equal(expected.Length, result);
+
+            AssertArrayContainsElements(nums, expected);
+        }
+
+        private void AssertArrayContainsElements(int[] actual, int[] expected)
+        {
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.Equal(expected[i], actual[i]);
+            }
+        }
+    }
+}
