9012ffdf88483eea808c6c883b6a0d6a6ff113e7
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-07-03 11:35:38 +0300

    1509. Minimum Difference Between Largest and Smallest Value in Three Moves

---
+++ LeetCode.Solutions/Medium/1509. Minimum Difference Between Largest and Smallest Value in Three Moves/MinDifference.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Medium._1509._Minimum_Difference_Between_Largest_and_Smallest_Value_in_Three_Moves
+{
+    public class MinDifference
+    {
+        //Complexity:
+        //Time complexity: O(n log n), где n это длина nums 
+        //Space complexity: O(1)
+
+        public int Solve(int[] nums)
+        {
+            if (nums.Length <= 4)
+                return 0;
+
+            Array.Sort(nums);
+
+            return Math.Min(Math.Min(nums[^1] - nums[3], nums[^2] - nums[2]), 
+                            Math.Min(nums[^3] - nums[1], nums[^4] - nums[0]));
+        }
+    }
+}
---
+++ LeetCode.Tests/Medium/1509. Minimum Difference Between Largest and Smallest Value in Three Moves/MinDifferenceTests.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.Solutions.Medium._1482._Minimum_Number_of_Days_to_Make_m_Bouquets;
+using LeetCode.Solutions.Medium._1509._Minimum_Difference_Between_Largest_and_Smallest_Value_in_Three_Moves;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Medium._1509._Minimum_Difference_Between_Largest_and_Smallest_Value_in_Three_Moves
+{
+    public class MinDifferenceTests
+    {
+        [Theory]
+        [InlineData(new int[] { 5, 3, 2, 4 }, 0)]
+        [InlineData(new int[] { 1, 5, 0, 10, 14 }, 1)]
+        [InlineData(new int[] { 3, 100, 20 }, 0)]
+        [InlineData(new int[] { 82, 81, 95, 75, 20 }, 1)]
+        public void TestMinDifference(int[] nums, int expected)
+        {
+            // Arrange
+            var solution = new MinDifference();
+
+            // Act
+            var result = solution.Solve(nums);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
