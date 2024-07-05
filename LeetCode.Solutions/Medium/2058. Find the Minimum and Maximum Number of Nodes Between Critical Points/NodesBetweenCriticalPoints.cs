172abc62abb1efbb86a101665aa25c72c05488c7
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-07-05 13:59:38 +0300

    2058. Find the Minimum and Maximum Number of Nodes Between Critical Points

---
+++ LeetCode.Solutions/Medium/2058. Find the Minimum and Maximum Number of Nodes Between Critical Points/NodesBetweenCriticalPoints.cs
@@ -0,0 +1,49 @@
+﻿using LeetCode.Solutions.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Medium._2058._Find_the_Minimum_and_Maximum_Number_of_Nodes_Between_Critical_Points
+{
+    public class NodesBetweenCriticalPoints
+    {
+        public int[] Solve(ListNode head)
+        {
+            if (head == null || head.next == null || head.next.next == null)
+            {
+                return [-1, -1];
+            }
+
+            int minDistance = int.MaxValue, maxDistance = 0;
+            int lastCritPos = -1, pos = 1;
+
+            var prev = head.val;
+            head = head.next;
+            
+            while(head.next != null)
+            {
+                if ((head.val > prev && head.val > head.next.val) ||
+                    (head.val < prev && head.val < head.next.val))
+                {
+                    if (lastCritPos != -1)
+                    {
+                        minDistance = Math.Min(minDistance, pos - lastCritPos);
+                        maxDistance += pos - lastCritPos;
+                    }
+
+                    lastCritPos = pos;
+                }
+
+                prev = head.val;
+                head = head.next;
+                pos++;
+            }
+
+
+            return minDistance != int.MaxValue 
+                ? [minDistance, maxDistance] : [-1, -1];
+        }
+    }
+}
---
+++ LeetCode.Tests/Medium/2058. Find the Minimum and Maximum Number of Nodes Between Critical Points/NodesBetweenCriticalPointsTests.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode.Solutions.Common;
+using LeetCode.Solutions.Medium._1552._Magnetic_Force_Between_Two_Balls;
+using LeetCode.Solutions.Medium._2058._Find_the_Minimum_and_Maximum_Number_of_Nodes_Between_Critical_Points;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Medium._2058._Find_the_Minimum_and_Maximum_Number_of_Nodes_Between_Critical_Points
+{
+    public class NodesBetweenCriticalPointsTests
+    {
+        [Theory]
+        [InlineData(new int[] { 3, 1 }, new int[] { -1, -1 })]
+        [InlineData(new int[] { 5, 3, 1, 2, 5, 1, 2 }, new int[] { 1, 3 })]
+        [InlineData(new int[] { 1, 3, 2, 2, 3, 2, 2, 2, 7 }, new int[] { 3, 3 })]
+        public void TestNodesBetweenCriticalPoints(int[] head, int[] expected)
+        {
+            // Arrange
+            var solution = new NodesBetweenCriticalPoints();
+
+            // Act
+            var result = solution.Solve(ListNode.FromArray(head));
+
+            // Assert
+            Assert.Equal(expected[0], result[0]);
+            Assert.Equal(expected[1], result[1]);
+        }
+    }
+}
