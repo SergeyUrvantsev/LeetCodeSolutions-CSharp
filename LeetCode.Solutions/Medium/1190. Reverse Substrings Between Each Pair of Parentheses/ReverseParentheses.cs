b9a4441bd60649c50f8d26eee30a58f05f473828
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-07-11 13:32:33 +0300

    1190. Reverse Substrings Between Each Pair of Parentheses

---
+++ LeetCode.Solutions/Medium/1190. Reverse Substrings Between Each Pair of Parentheses/ReverseParentheses.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Medium._1190._Reverse_Substrings_Between_Each_Pair_of_Parentheses
+{
+    public class ReverseParentheses
+    {
+        //Complexity:
+        //Time complexity: O(n)
+        //Space complexity: O(n)
+        public string Solve(string s)
+        {
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (s[i] == ')')
+                {
+                    var start = stack.Pop();
+                    var reversed = new string(s.Substring(start + 1, i - start - 1).Reverse().ToArray());
+                    s = s.Substring(0, start) + reversed + s.Substring(i + 1);
+                    i = start + reversed.Length - 1;
+                }
+            }
+
+            return s;
+        }
+    }
+}
---
+++ LeetCode.Tests/Medium/1190. Reverse Substrings Between Each Pair of Parentheses/ReverseParenthesesTests.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.Solutions.Medium._1190._Reverse_Substrings_Between_Each_Pair_of_Parentheses;
+using LeetCode.Solutions.Medium._1509._Minimum_Difference_Between_Largest_and_Smallest_Value_in_Three_Moves;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Medium._1190._Reverse_Substrings_Between_Each_Pair_of_Parentheses
+{
+    public class ReverseParenthesesTests
+    {
+        [Theory]
+        [InlineData("(abcd)", "dcba")]
+        [InlineData("(u(love)i)", "iloveu")]
+        [InlineData("(ed(et(oc))el)", "leetcode")]
+        [InlineData("sxmdll(q)eki(x)", "sxmdllqekix")]
+        public void TestReverseParentheses(string nums, string expected)
+        {
+            // Arrange
+            var solution = new ReverseParentheses();
+
+            // Act
+            var result = solution.Solve(nums);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
