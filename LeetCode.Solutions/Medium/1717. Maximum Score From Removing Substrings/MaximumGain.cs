9bb0ee526cfaacd47c75d6bb442540ece1064f0f
SERGEYURV\ssurv <s.s.urvantsev@gmail.com>
2024-07-12 12:43:01 +0300

    1717. Maximum Score From Removing Substrings

---
+++ LeetCode.Solutions/Medium/1717. Maximum Score From Removing Substrings/MaximumGain.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Medium._1717._Maximum_Score_From_Removing_Substrings
+{
+    public class MaximumGain
+    {
+        //Complexity 
+        //Time: O(n), где n - длина s
+        //Space: O(1)
+
+        public int Solve(string s, int x, int y)
+        {
+            int result = 0;
+
+            if (x > y)
+            {
+                result += RemoveAndScore('a', 'b', x);
+                result += RemoveAndScore('b', 'a', y);
+            }
+            else
+            {
+                result += RemoveAndScore('b', 'a', y);
+                result += RemoveAndScore('a', 'b', x);
+            }
+
+            return result;
+
+            int RemoveAndScore(char first, char second, int points)
+            {
+                var stack = new Stack<char>();
+                int score = 0;
+
+                foreach (var c in s)
+                {
+                    if (stack.Count > 0 && stack.Peek() == first && c == second)
+                    {
+                        stack.Pop();
+                        score += points;
+                    }
+                    else
+                    {
+                        stack.Push(c);
+                    }
+                }
+
+                var arr = stack.ToArray();
+                Array.Reverse(arr);
+                s = new(arr);
+
+                return score;
+            }
+        }
+    }
+}
---
+++ LeetCode.Tests/Medium/1717. Maximum Score From Removing Substrings/MaximumGainTests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Solutions.Medium._1552._Magnetic_Force_Between_Two_Balls;
+using LeetCode.Solutions.Medium._1717._Maximum_Score_From_Removing_Substrings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests.Medium._1717._Maximum_Score_From_Removing_Substrings
+{
+    public class MaximumGainTests
+    {
+        [Theory]
+        [InlineData("cdbcbbaaabab", 4, 5, 19)]
+        [InlineData("aabbaaxybbaabb", 5, 4, 20)]
+        public void TestMaximumGain(string s, int x, int y, int expected)
+        {
+            // Arrange
+            var solution = new MaximumGain();
+
+            // Act
+            var result = solution.Solve(s, x, y);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
